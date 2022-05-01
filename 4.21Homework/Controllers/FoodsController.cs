720a5c7f52b540b414cfd012b31995b718150750
timlan0214 <timlan8888@gmail.com>
2022-05-01 15:01:20 +0800

    4/26作業

--- 4.21Homework/Controllers/FoodsController.cs
+++ 4.21Homework/Controllers/FoodsController.cs
@@ -145,6 +145,55 @@ namespace _4._21Homework.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Search()
+        {
+            ViewData["Message"] = "英雄搜尋 GET => 取得表單";
+            return View(new BlueSearchViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Search([Bind("Name,MinStare,MaxStare")] FoodSearchParams searchParams)
+        {
+
+            var viewModel = new BlueSearchViewModel(); // 清出記憶體空間 => 打掃房間
+            if (ModelState.IsValid)
+            {
+                var searchResult = _context.Foods.ToList(); // 取出所有資料
+                if (!string.IsNullOrEmpty(searchParams.Name)) // 如果有輸入名字，就用名字當條件搜尋
+                {
+                    searchResult = searchResult
+                        .Where(x => x.Name == searchParams.Name)
+                        .ToList();
+                }
+
+                // 如果攻擊力的範圍合邏輯，就用加攻擊力條件再多篩一次
+                if (searchParams.MinStars >= 0
+                    && searchParams.MaxStars > 0
+                    && searchParams.MinStars < searchParams.MaxStars)
+                {
+                    // 最小 ATK = 10；最大 ATK = 20
+                    // ==> 那些英雄攻擊歷介在 10~20 之間
+                    searchResult = searchResult
+                        .Where(x => x.Stars >= searchParams.MinStars && x.Stars <= searchParams.MaxStars)
+                        .ToList();
+                }
+                else // 如果攻擊力的範圍不合邏輯，就顯示忽略攻擊力條件字樣
+                {
+                    ViewData["Message"] = $"（忽略星星搜尋條件）";
+                }
+
+                // 用 ViewData 讓 Controller 與 View 共享資料
+                ViewData["Message"] += $"搜尋到 {searchResult.Count} 個星星";
+
+                // 把搜尋條件與搜尋結果賦值到 ViewModel 的 property
+                // MVC 才有辦法幫我們把資料打包進 Search.cshtml
+                viewModel.SearchParams = searchParams;
+                viewModel.Foods = searchResult;
+            }
+            // 回傳 View + ViewModel 進行打包作業
+            return View(viewModel);
+        }
         private bool FoodExists(int id)
         {
             return _context.Foods.Any(e => e.Id == id);
